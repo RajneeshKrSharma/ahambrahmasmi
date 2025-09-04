78800ef1d227972d68d06e48aaa89db7b802cd6b
RajneeshKrSharma <rajneeshsharma199@gmail.com>
2025-09-04 06:10:11 +0530

    refactor(setting): enable debug to true

--- ahambrahmasmi/settings.py
+++ ahambrahmasmi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rmr=i&@q9yc1)n!dym#b&*&mbamt1eurr+q51rwnymyi35sg9*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["ahambrahmasmi.pythonanywhere.com", "127.0.0.1"]
 
